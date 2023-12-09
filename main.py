703137e1b78eea52d9596c1442a7181e5c2586ac
xfarkasp <xfarkasp@stuba.sk>
2023-12-09 16:01:30 +0100

    3 networks implemented

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import torch.optim as optim
 from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import fetch_openml
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 import torchvision.transforms as transforms
 from PIL import Image
 import matplotlib.pyplot as plt
@@ -12,38 +12,37 @@ import numpy as np
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
-save_path = "C:\\Users\\pedro\\PycharmProjects\\ui_3D\\models\\"
+def metric_calc(model_name,true_labels, predicted_labels, conf_matrix):
+    # conf metrix visualization
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(conf_matrix, annot=True, fmt="d", cmap="Blues", cbar=False)
+    plt.xlabel("Predicted Label")
+    plt.ylabel("True Label")
+    plt.title("Confusion Matrix")
+    plt.show()
 
-# Set random seed for reproducibility
-seed = 42
-np.random.seed(seed)
-torch.manual_seed(seed)
+    accuracy = accuracy_score(true_labels, predicted_labels) * 100
+    precision = precision_score(true_labels, predicted_labels, average='weighted') * 100
+    recall = recall_score(true_labels, predicted_labels, average='weighted') * 100
+    f1 = f1_score(true_labels, predicted_labels, average='weighted') * 100
 
-# Load the MNIST dataset
-mnist = fetch_openml('mnist_784')
-X, y = mnist.data.astype('float32'), mnist.target.astype('int64')
+    metrics = ['Accuracy' ,'Precision', 'Recall', 'F1-Score']
+    values = [accuracy, precision, recall, f1]
 
-# Normalize pixel values to be between 0 and 1
-X /= 255.0
+    plt.figure(figsize=(12, 6))
 
-# Split the dataset into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    plt.bar(metrics, values, color=['blue', 'red', 'green', 'orange'])
+    plt.ylim(0, 100)  # Assuming values are between 0 and 1
 
-# Convert NumPy arrays to PyTorch tensors
-X_train_tensor = torch.from_numpy(X_train.values).float()
-y_train_tensor = torch.from_numpy(y_train.values).long()
+    for i, value in enumerate(values):
+        plt.text(i, value + 0.05, f'{value:.4f}', ha='center', va='center', color='black')
 
-X_test_tensor = torch.from_numpy(X_test.values).float()
-y_test_tensor = torch.from_numpy(y_test.values).long()
+    plt.xlabel("Metrics")
+    plt.ylabel("Values")
+    plt.title(model_name)
+    plt.show()
 
-# Create DataLoader for training and testing sets
-train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
-test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
-
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
-
-# Model 1: Simple Feedforward Neural Network
+# Feedforward
 class Feed_Forward(nn.Module):
 
     def __init__(self):
@@ -52,6 +51,7 @@ class Feed_Forward(nn.Module):
         self.fc1 = nn.Linear(28 * 28, 128)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(128, 10)
+        self.model_name = "FeedForward"
 
     def forward(self, x):
         x = self.flatten(x)
@@ -60,9 +60,7 @@ class Feed_Forward(nn.Module):
         x = self.fc2(x)
         return x
 
-feed_forward = Feed_Forward()
-
-# Model 2: Convolutional Neural Network
+# Cnn
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
@@ -70,8 +68,9 @@ class CNN(nn.Module):
         self.relu = nn.ReLU()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(14 * 14 * 32, 128)
+        self.fc1 = nn.Linear(32 * 14 * 14, 128)  # Adjusted input size
         self.fc2 = nn.Linear(128, 10)
+        self.model_name = "CNN"
 
     def forward(self, x):
         x = self.conv1(x)
@@ -83,12 +82,11 @@ class CNN(nn.Module):
         x = self.fc2(x)
         return x
 
-model2 = CNN()
 
-# Model 3: Deep Feedforward Neural Network
-class DeepNN(nn.Module):
+# Deep neural network
+class Deep(nn.Module):
     def __init__(self):
-        super(DeepNN, self).__init__()
+        super(Deep, self).__init__()
         self.flatten = nn.Flatten()
         self.fc1 = nn.Linear(28 * 28, 256)
         self.relu1 = nn.ReLU()
@@ -97,6 +95,7 @@ class DeepNN(nn.Module):
         self.fc3 = nn.Linear(128, 64)
         self.relu3 = nn.ReLU()
         self.fc4 = nn.Linear(64, 10)
+        self.model_name = "Deep"
 
     def forward(self, x):
         x = self.flatten(x)
@@ -109,14 +108,7 @@ class DeepNN(nn.Module):
         x = self.fc4(x)
         return x
 
-model3 = DeepNN()
 
-# Define the loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer1 = optim.Adam(feed_forward.parameters(), lr=0.001)
-
-
-# Train the models
 def train_model(model, optimizer, train_loader, epochs=5):
     for epoch in range(epochs):
         model.train()
@@ -127,13 +119,8 @@ def train_model(model, optimizer, train_loader, epochs=5):
             loss.backward()
             optimizer.step()
         print(f'Epoch {epoch + 1}/{epochs}, Loss: {loss.item()}')
-    torch.save(model.state_dict(), 'model_state_dict.pth')
 
 
-# Training each model
-train_model(feed_forward, optimizer1, train_loader)
-
-# Evaluate the models on the test set
 def evaluate_model(model, test_loader):
     model.eval()
     all_predictions = []
@@ -147,35 +134,16 @@ def evaluate_model(model, test_loader):
             all_labels.extend(labels.numpy())
 
     accuracy = accuracy_score(all_labels, all_predictions)
-    return accuracy, all_labels, all_predictions
-
-# Assuming 'feed_forward' is your trained model and 'test_loader' is your DataLoader
-feed_forward_accuracy, true_labels, predicted_labels = evaluate_model(feed_forward, test_loader)
-
-print(f'Feed Forward Accuracy: {feed_forward_accuracy * 100:.2f}%')
+    print(f"Accuracy score is: {accuracy}%")
+    conf_matrix = confusion_matrix(all_labels, all_predictions)
+    metric_calc(model.model_name, all_labels, all_predictions, conf_matrix)
 
 
-def class_accuracy(true_labels, predicted_labels, num_classes):
-    class_acc = []
-    for i in range(num_classes):
-        class_true = np.array(true_labels) == i
-        class_pred = np.array(predicted_labels) == i
-        acc = accuracy_score(class_true, class_pred)
-        class_acc.append(acc)
-    return class_acc
+def classify(model):
+    path = input("Path to image or 0 for return: ")
+    if path == '0':
+        return
 
-class_accuracies = class_accuracy(true_labels, predicted_labels, num_classes=10)
-
-# Bar Chart for Overall Accuracy
-plt.figure(figsize=(8, 6))
-sns.barplot(x=['Feed Forward'], y=[feed_forward_accuracy], color='skyblue')
-plt.ylim(0, 1)  # Set y-axis limit to represent accuracy percentage
-plt.ylabel("Accuracy")
-plt.title("Overall Accuracy")
-plt.show()
-
-def clasify(model):
-    path = input("Path to image: ")
     image = Image.open(path).convert('L')  # Convert to grayscale
     transform = transforms.Compose([transforms.ToTensor()])
     input_image = transform(image).unsqueeze(0)  # Add batch dimension
@@ -188,8 +156,120 @@ def clasify(model):
     print(f'The predicted class is: {predicted_class.item()}')
     # Show the image
     plt.imshow(image, cmap='gray')
-    plt.title(f'Predicted Class: {predicted_class.item()}')
+    plt.title(f'Predicted Number: {predicted_class.item()}')
     plt.show()
 
-while True:
-    clasify(feed_forward)
+
+if __name__ == "__main__":
+    save_path = "C:\\Users\\pedro\\PycharmProjects\\ui_3D\\models\\"
+
+    # Set random seed for reproducibility
+    seed = 42
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    # Load the MNIST dataset
+    mnist = fetch_openml('mnist_784')
+    X, y = mnist.data.astype('float32'), mnist.target.astype('int64')
+
+    # Normalize pixel values to be between 0 and 1
+    X /= 255.0
+
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Convert NumPy arrays to PyTorch tensors
+    # X_train_tensor = torch.from_numpy(X_train.values).float()
+    X_train_tensor = torch.from_numpy(X_train.values.reshape(-1, 1, 28, 28)).float()
+    y_train_tensor = torch.from_numpy(y_train.values).long()
+
+    # X_test_tensor = torch.from_numpy(X_test.values).float()
+    X_test_tensor = torch.from_numpy(X_test.values.reshape(-1, 1, 28, 28)).float()
+    y_test_tensor = torch.from_numpy(y_test.values).long()
+
+    # Create DataLoader for training and testing sets
+    train_dataset = TensorDataset(X_train_tensor, y_train_tensor)
+    test_dataset = TensorDataset(X_test_tensor, y_test_tensor)
+
+    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+    current_model = None
+
+    while True:
+        print(
+            "Menu:\n 1.Train model\n 2.Load model\n 3.Test model\n 4. Train and Test model\n 5.Classify snipets\n 6.Save model")
+        user_input = input("Input: ")
+        if user_input == '1':
+            print("Models:\n 1. Feed Forward \n 2. CNN \n 3. Deep")
+            model_select = input("Chosen model: ")
+            if model_select == '1':
+                model = Feed_Forward()
+
+                # loss function and optimizer
+                criterion = nn.CrossEntropyLoss()
+                optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+                train_model(model, optimizer, train_loader)
+                current_model = model
+            elif model_select == '2':
+                model = CNN()
+
+                # loss function and optimizer
+                criterion = nn.CrossEntropyLoss()
+                optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+                train_model(model, optimizer, train_loader)
+                current_model = model
+
+            elif model_select == '3':
+
+                X_test_tensor = torch.from_numpy(X_test.values.reshape(-1, 1, 28, 28)).float()
+                y_test_tensor = torch.from_numpy(y_test.values).long()
+
+                model = Deep()
+
+                # loss function and optimizer
+                criterion = nn.CrossEntropyLoss()
+                optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+                train_model(model, optimizer, train_loader)
+                current_model = model
+
+        # elif user_input == '2':
+        #     load_path = input("Path to model: ")
+        #     current_model = torch.load(load_path)
+
+        elif user_input == '3':
+            if current_model != None:
+                evaluate_model(current_model, test_loader)
+            else:
+                print("No model was trained or loaded")
+
+        elif user_input == '4':
+            print("Models:\n 1. Feed Forward \n 2. CNN \n 3. Deep")
+            model_select = input("Chosen model: ")
+            if model_select == '1':
+                model = Feed_Forward()
+
+                # loss function and optimizer
+                criterion = nn.CrossEntropyLoss()
+                optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+                train_model(model, optimizer, train_loader)
+                current_model = model
+                evaluate_model(current_model, test_loader)
+
+        elif user_input == '5':
+            while True:
+                classify(current_model)
+
+        elif user_input == '6':
+            if current_model != None:
+                save_path = input("Path to save model")
+                torch.save(current_model.state_dict(), save_path + current_model.model_name +'.pth')
+            else:
+                print("No model was trained or loaded")
+
+        elif user_input == '0':
+            break
